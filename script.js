3b380e2e3fb961abd4127c59de3f8994a2241540
Zharakamkar <zahrakamkar203@gmail.com>
2023-01-26 07:16:17 +0330

    feature JS

---
+++ script.js
@@ -0,0 +1,17 @@
+const massage = document.querySelector(".massage");
+function shownNotification(item) {
+    const styleTop = item.top;
+    const styleRight = item.right;
+    massage.style.top = `${styleTop}px`;
+    massage.style.right = `${styleRight}px`;
+    massage.innerHTML = item.html;
+    massage.classList.add = item.className;
+    console.log(massage.classList);
+}
+
+shownNotification({
+    top: +prompt("top"),
+    right: +prompt("right"),
+    html: prompt("write sth"),
+    className: prompt("Class"),
+});
