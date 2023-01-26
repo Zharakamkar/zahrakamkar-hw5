0fc751c4198a608a40d6b6ee035ea43ab4bb80ee
Zharakamkar <zahrakamkar203@gmail.com>
2023-01-26 07:25:34 +0330

    feature JS

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const ask = (question, yes, no) => {
+    if (confirm(question)) yes();
+    else no();
+}
+
+ask('do you agree?', () => { alert('you agreed .'); }, () => { alert('you canceled the execution.'); })
